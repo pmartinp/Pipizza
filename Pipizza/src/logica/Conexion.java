7ae89171f2495d8c243d5bad4c70fc3d125f13d5
pmartinp <102436420+pmartinp@users.noreply.github.com>
2022-06-08 09:26:43 +0200

    Add files via upload

--- Pipizza/src/logica/Conexion.java
+++ Pipizza/src/logica/Conexion.java
@@ -1,32 +1,38 @@
 package logica;
 
+import java.beans.Statement;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class Conexion {
-	//driver JDBC
-	private static final String DRIVER = "com.mysql.cj.jdbc.Driver";
-	//direcciï¿½n de la BD MySQL
-	private static final String BBDD = "jdbc:mysql://localhost:3306/pipizza";
-	//ususario y contraseï¿½a de acceso a la BD
-	private static final String USUARIO = "root";
-	private static final String PASSWORD = "";
-	
-	public Conexion() {}
-	public Connection conexionBBDD() {
-		Connection conec = null;
-		//Controlamos las excepciones que aparecen al interactuar con la BBDD
-		try {
-			Class.forName(DRIVER);
-			//Crear una conexiï¿½n a la Base de Datos
-			conec = DriverManager.getConnection(BBDD, USUARIO, PASSWORD);
-		} catch (Exception errores) {
-			//Control de errores de la conexiï¿½n a la BBDD
-			System.err.println("Se ha producido un error al conectar con la Base de Datos.\n" + errores);
-		}
-		return conec;
-	}
+	    public Connection con;
+	    private  final String driver="com.mysql.jdbc.Driver";
+	    private  final String user="root";
+	    private  final String pass="";
+	    private  final String url="jdbc:mysql://localhost:3306/centrosalud";
+	    private java.sql.Statement statement;
+	    //public Usuario usuario= new Usuario();
+	    
+	    public Conexion(){
+	        connect();
+	    }
+
+	    public Connection connect() {
+	        // Reseteamos a null la conexion a la bd
+	        con = null;
+	        try{
+	            Class.forName(driver);
+	            // Nos conectamos a la bd
+	            con= (Connection) DriverManager.getConnection(url, user, pass);
+	            System.err.println("conectado!!!!!!!!!!!!");
+	        }
+	        // Si la conexion NO fue exitosa mostramos un mensaje de error
+	        catch (ClassNotFoundException | SQLException e){
+	            System.err.println("Error en la base de datos: "+e);
+	        }return con;
+	    }
 	
 	public void cerrarConexion(Connection conection) {
 		try {
@@ -37,5 +43,82 @@ public class Conexion {
 			System.err.println("Se ha producido un error al conectar con la Base de Datos." + e);
 		}
 	}
+	
+	//Insertamos un usuario en la base de datos 
+    public boolean RegistrarUsuario(String cadena){
+        try{
+            statement = con.createStatement();
+            cadena = "INSERT INTO usuario (nombre,apellido1,apellido2,dni,contraseña,saldo) VALUES ("+cadena+")";
+            statement.executeUpdate(cadena);
+            return true;
+        }
+        catch (SQLException e){
+            System.err.println("Error en la base de datos: "+e);
+            return false;
+        }
+    }
+    
+  //Para iniciar sesion    
+    public boolean HacerLogin(String dni, String contrasena){
+      try{
+           statement = con.createStatement(ResultSet.TYPE_SCROLL_SENSITIVE, ResultSet.CONCUR_READ_ONLY);
+           String sql=("SELECT dni,contraseña FROM usuario WHERE  dni="+dni+" and contraseña=" +contrasena);
+           System.out.println(sql);
+           ResultSet rs = statement.executeQuery(sql);
+           
+           if (rs.next()){
+                return true;
+            }else{
+               return false;
+           }
+           
+       }catch (SQLException e){
+           System.err.println("Error en la base de datos: "+e);
+           return false;
+       }
+   }
+    
+  //Una vez que hace el login guardar datos del usuario
+    public Usuario cargarusuario(String dni){
+         try{
+            statement = con.createStatement(ResultSet.TYPE_SCROLL_SENSITIVE, ResultSet.CONCUR_READ_ONLY);
+            String sql=("SELECT * FROM usuario WHERE  dni="+dni);
+            System.out.println(sql);
+            ResultSet rs = statement.executeQuery(sql);
+            
+            Usuario usuario= new Usuario();
+            
+            if (rs.next()){
+                usuario.setNombre(rs.getString("nombre"));
+                usuario.setDNI(rs.getString("dni"));
+                usuario.setApellido1(rs.getString("apellido1"));
+                usuario.setApellido2(rs.getString("apellido2"));
+                usuario.setDinero(rs.getInt("saldo"));
+             }
+            return usuario;
+        }catch (SQLException e){
+            System.err.println("Error en la base de datos: "+e);
+            return null;
+        }
+    }
+
+//Compruebo si el usuario ha sido registrado.
+    public boolean ComprobarRegistrado(String prueba){
+       try{
+            statement = con.createStatement(ResultSet.TYPE_SCROLL_SENSITIVE, ResultSet.CONCUR_READ_ONLY);
+            String sql=("SELECT dni FROM usuario WHERE dni LIKE "+prueba);
+            ResultSet rs = statement.executeQuery(sql);
+          
+            if (rs.next()){
+                 return true;
+             }else{
+                return false;
+            }
+            
+        }catch (SQLException e){
+            System.err.println("Error en la base de datos: "+e);
+            return false;
+        }
+    }
 	
 }
